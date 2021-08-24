80f0d720d9169b241c2fae3a95750cde36dd0acb
Nhien Dang <danghaonhien@gmail.com>
2021-08-24 11:40:25 -0700

    first commit

---
+++ app/data/QuizData.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-undef */
+export default data = [
+  {
+    question: 'How would you rate My-Codi experience?',
+    options: ['Excellent', 'Okay', 'Terrible'],
+  },
+  {
+    question: 'Do you use My-Codi frequently?',
+    options: ['Yes', 'No'],
+  },
+  {
+    question: 'Do you recommend My-Codi frequently?',
+    options: ['Yes', 'No'],
+  },
+  {
+    question: 'Tell us what do you enjoy about My-Codi??',
+    options: [''],
+  },
+];
